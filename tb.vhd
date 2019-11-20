086441ee3cc19f2d1dc7388786d93a212eaa4db6
Jaydeepkakadiya19 <38078193+Jaydeepkakadiya19@users.noreply.github.com>
2019-11-21 02:14:59 +0530

    Update tb.vhd

--- tb.vhd
+++ tb.vhd
@@ -41,7 +41,7 @@ ARCHITECTURE behavior OF tb IS
  
     COMPONENT IO
     PORT(
-	     clk : in std_logic;
+	   
 		  clk1 : in std_logic;
 		  
          bcd_in : in std_logic_vector(3 downto 0);
@@ -54,22 +54,21 @@ ARCHITECTURE behavior OF tb IS
    -- No clocks detected in port list. Replace <clock> below with 
    -- appropriate port name 
       
-signal	     clk :  std_logic;
 signal	     clk1 :  std_logic;
 
       signal   bcd_in : std_logic_vector(3 downto 0);
 	signal		ans :  std_logic_vector(31 downto 0);
   signal	sout :  std_logic;
 		signal	oflow: std_logic;
-   constant clk_period : time := 200 ns;
-	constant clk1_period : time := 1 ns;
+   
+	constant clk1_period : time := 500 ps;
 	
  
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
    uut: IO PORT MAP (
-	   clk =>clk,
+	  
 		clk1=>clk1,
 	     bcd_in => bcd_in,
 			ans => ans,
@@ -78,14 +77,7 @@ BEGIN
         );
 
    -- Clock process definitions
-   clk_process :process
-   begin
-		clk <= '0';
-		wait for clk_period/2;
-		clk <= '1';
-		wait for clk_period/2;
-   end process;
-   
+  
 	clk1_process :process
    begin
 		clk1 <= '0';
@@ -99,33 +91,35 @@ BEGIN
    begin		
 	 -- if(rising_edge(clk)) then
       -- hold reset state for 100 ns.
-      wait for 100 ns;	
-        bcd_in<="1010";
-		wait for 100 ns;	
+   	wait for 100 ns;  
+     
+    	  bcd_in<="1010";
+		wait for 50 ns;	
         bcd_in<="0011";
-	   wait for 100 ns;	
+		 wait for 50 ns;	
          bcd_in<="0101";		
-		wait for 100 ns;	
-        bcd_in<="0011";
-		wait for 100 ns;	
-     bcd_in<="0001";
-		 
-   	 wait for 100 ns;	
-        bcd_in<="1100";
-		
-		wait for 100 ns;	
-        bcd_in<="1010";
-	  
-	
-	 wait for 100 ns;	
+		wait for 50 ns;	
+        bcd_in<="0011";  
+	   wait for 50 ns;	
+         bcd_in<="0101";		
+	 	 wait for 50 ns;	
+   
+    	bcd_in<="1100";         
+        wait for 50 ns;
+        bcd_in<="1111";
+    	wait for 1 ns;	
+    
+
+      bcd_in<="1010";
+	 	 wait for 50 ns;	
         bcd_in<="0011"; 
-		 wait for 100 ns;	
+		 wait for 50 ns;	
         bcd_in<="0111";
-	--	 wait for 100 ns;	
---        bcd_in<="0101";
+		 wait for 50 ns;	
+        bcd_in<="0101";
 --		 wait for 100 ns;	
 --        bcd_in<="0101";
-		
+   wait;		
 	--	end if;
 	 
       -- insert stimulus here 
